f6ed074004ac5a4ec02fb6b84e0b172e5afcb521
btousifar <bn.tousifar86@gmail.com>
2024-10-14 23:56:46 -0500

    Complete the calculator logic for single operand
    [Ticket: 3]

--- script.js
+++ script.js
@@ -15,14 +15,15 @@ function divide(a, b) {
 }
 
 
-console.log(divide(2,3))
-
-let firstNum;
+let val = '';
+let numval = 0;
+let firstNum ='';
 let secondNum;
 let operator;
 let result;
 let firstNumStr;
 let secondNumStr;
+let count = 0;
 
 
 function operate(operator, firstNum, secondNum) {
@@ -43,70 +44,104 @@ const btnContainer = document.querySelector('.buttons');
 const display = document.querySelector('.display');
 const displayBox = document.querySelector('.display-box')
 const equalSign = document.querySelector('#btnE')
+const clearBtn = document.querySelector('#btnC')
 
 btnContainer.addEventListener('click', (e) => {
     const clickedButton = e.target
-    // console.log(clickedButton)
-    // console.log(clickedButton.textContent)
 
-    // if (clickedButton.id === "+") {
-    //     firstNum = displayBox.textContent;
-    //     operator = clickedButton.id;
-    //     console.log(`first number: ${firstNum}`);
-    //     console.log(`operator: ${operator}`)
-    // }
-
-    if (!(clickedButton.textContent === "=")) {
-        displayBox.textContent += clickedButton.textContent
-        console.log(typeof(displayBox.textContent))
-
-        const displayString = displayBox.textContent;
-        console.log(displayString)
-
-        const displayArr = displayString.split('')
-        console.log(displayArr)
-
-        const operatorIndex = displayArr.findIndex(item => item === "+");
-        console.log(operatorIndex)
-
-        firstNum = displayArr.slice(0, operatorIndex)
-        console.log(firstNum)
-        secondNum = displayArr.slice(operatorIndex+1)
-        console.log(secondNum)
-
-        operator = displayArr[operatorIndex]
-        console.log(operator)
-
-        // put back the numbers array to string
-        firstNumStr = firstNum.join("")
-        secondNumStr = secondNum.join("")
-        console.log(firstNumStr)
-        console.log(secondNumStr)
-        
-        
-
-        // add event listener when '=' is clicked
+    // if clicked button is a number
+    if (!isNaN(clickedButton.textContent)) {
+        val += clickedButton.textContent;
+        displayBox.textContent = val;
     }
     
+    // if click button is an operator
+    if (clickedButton.textContent === "+" || clickedButton.textContent === "-" || clickedButton.textContent === "*" || clickedButton.textContent === "/") {
+        if (firstNum === '') {
+            firstNum = val;
+            val = '';
+            operator = clickedButton.textContent;
+            console.log(`operator: ${operator}`)
+        }
+        // when we are at inserting next number
+        else if (firstNum !== '' && val !== '') {
+            secondNum = val;
+            result = operate(operator, parseFloat(firstNum), parseFloat(secondNum))
+            console.log(result)
+            displayBox.textContent = result
+
+            firstNum = result
+            val = '';
+            operator = clickedButton.textContent;
+            
+        }
+    }
 })
-
 equalSign.addEventListener('click', () => {
-    result = operate(operator, +firstNumStr, +secondNumStr);
-    console.log(result)
+    secondNum = val
+    
+    if (firstNum !== "") {
+        result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
+        displayBox.textContent = result
+    }
 
-    // displayBox.innerHTML = '';
-
-
-    const displayResult = document.createElement("div");
-    displayResult.classList.add("display-result");
-    displayResult.textContent = `=${result}`;
-
-    displayBox.append(document.createElement('br'));
-
-
-    displayBox.append(displayResult);
 }) 
 
+clearBtn.addEventListener('click', () => {
+    val = '';
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    result = '';
+    displayBox.textContent = 0
+})
+        // when equal button is clicked
 
 
+
+
+        // val += clickedButton.textContent;
+        // numval = parseInt(val)
+        // console.log(numval)
+    
+//     else if (count === 1) {
+//         firstNum = numval;
+//         console.log(`firstNum: ${firstNum}`)
+//         val = ''
+//         operator = clickedButton.textContent;
+        
+//     }
+//     else {
+//         secondNum = numval;
+//         console.log(`secondtNum: ${secondNum}`)
+
+//         firstNum = operate(operator, +firstNum, +secondNum)
+//         console.log(`firstNum: ${firstNum}`)
+//     }
+    
+
+    
+    
+    
+
+
+equalSign.addEventListener('click', () => {
+    // result = operate(operator, +firstNumStr, +secondNumStr);
+    // console.log(result)
+
+    // // displayBox.innerHTML = '';
+
+
+    // const displayResult = document.createElement("div");
+    // displayResult.classList.add("display-result");
+    // displayResult.textContent = `=${result}`;
+
+    // displayBox.append(document.createElement('br'));
+
+
+    // displayBox.append(displayResult);
+
+
+
+})
     
